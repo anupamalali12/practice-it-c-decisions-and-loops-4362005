e64a0fc03fec2b14536f91fbd7c327b1d0761eb1
anupamalali12 <130979033+anupamalali12@users.noreply.github.com>
2023-04-18 11:15:41 +0530

    Update 01_11-tryit.c

--- CH01/01_11-tryit.c
+++ CH01/01_11-tryit.c
@@ -8,11 +8,27 @@ int main()
 
 	/* seed the randomizer */
 	srand( (unsigned)time(NULL) );
+	
 
 	/* grab a random card, 13 to a suit */
 	card = rand() % 13;		/* range is 0 to 12 */
 
-	printf("You drew card %d\n",card);
+	printf("You drew card:");
+	Switch(card)
+	{
+		case 0:
+		printf("Ace\n");
+		break;
+		case 10:
+		printf("Jack\n");
+		break;
+		case 11:
+		printf("Queen\n");
+		break;
+		default:
+		printf("%d\n",card);
+		       
+	}
 
 	return(0);
 }
